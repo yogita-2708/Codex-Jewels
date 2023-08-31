d1a725788a5157d5e578b0f66df696c503e27f22
Stuti Biswal <140154359+stutibiswal01@users.noreply.github.com>
2023-08-31 09:02:15 +0530

    FindStore.jsx
     file uploaded

---
+++ FindStore.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import Footer from "./Footer";
+import "./FindStore.css";
+import {
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    AlertDescription,
+    CloseButton,
+    Button
+  } from '@chakra-ui/react'
+import Navbar from "../Pages/Navbar";
+const Appointment = () => {
+    const [display, setDisplay] = useState("none");
+    return(<>
+        <Navbar/> 
+        <div>
+            <div className="mainDiv">
+                <img src="https://www.avtaara.com/wp-content/uploads/2020/04/5e8b141d1c67e.png.webp" alt="" />
+                <div className="mainDiv-2"><h1>Find a store near you!!!</h1>
+                <input type="number" required="true" placeholder="Enter Pincode" /><br />
+                <button onClick={() => setDisplay('')} ><h4>submit</h4></button></div>
+                <Alert
+            status='success'
+            variant='subtle'
+            flexDirection='column'
+            alignItems='center'
+            justifyContent='center'
+            textAlign='center'
+            height='200px'
+            display={display}
+            >
+            <AlertIcon boxSize='40px' mr={0} />
+            <AlertTitle mt={4} mb={1} fontSize='lg'>
+                Your Pin is Recorded!
+            </AlertTitle>
+            <AlertDescription maxWidth='sm'>
+                Thanks for submitting your Pin. Our team will get back to you soon.
+            </AlertDescription>
+        </Alert>
+            </div>
+        </div>
+        <div className="footer1">
+            <Footer/>
+        </div>
+        
+        </>
+    )
+}
+
+export default Appointment;
