8952fb2019d3283a547f17cf04f264d25c910055
AnusiktaMohanty20 <133092859+AnusiktaMohanty20@users.noreply.github.com>
2023-08-31 09:00:11 +0530

    CheckOut.jsx
    updated

---
+++ CheckOut.jsx
@@ -0,0 +1,57 @@
+// title,description,type,weight,price,disPrice,img,brand
+
+import React, { useState, useEffect } from "react";
+//import { useHistory } from 'react-router-dom';
+
+function CheckOut() {
+  const [products, setProducts] = useState([]);
+  //const [cart, setCart] = useState([]);
+
+    const handleCheckout = () => {
+      //history.push('/payment');
+      window.location.href = '/payment';
+    };
+
+  async function fetchData() {
+    let res = await fetch("http://localhost:5000/Product");
+    let data = await res.json();
+    setProducts(data);
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const calculateTotalPrice = () => {
+    let total = 0;
+    for (const item of products) {
+      total += item.DiscountPrice; //multply with quantity
+    }
+    return total;
+  };
+
+  return (
+    <div>
+      <h1>Cart</h1>
+      <div>
+        {products.map((el) => {
+          return (
+            <div key={products.id}>
+              <h4>{el.Type}</h4>
+              <div>
+                <ul>
+                  <li key={el.id}>
+                    {el.Title} - ${el.DiscountPrice}
+                  </li>
+                </ul>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <p>Total: ${calculateTotalPrice()}</p>
+      <button onClick={handleCheckout}>Checkout</button>
+    </div>
+  );
+}
+
+export default CheckOut;
